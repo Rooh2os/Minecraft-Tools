1b7a9d48a5877494aa2d4411ffdbadfbc89a8f31
Rooh2os <117789258+Rooh2os@users.noreply.github.com>
2025-07-17 15:38:19 -0400

    did stuff

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import basic,math
+import basic
 from pythonping import ping as pping
 
 
@@ -10,17 +10,32 @@ def ping(host,cnt,tmot):
         print(f"DEBUG: {exc}")
         return False
 
-while True:
-    try:
-        input = int(input("1: Ping server\n2: Calculate # of items into stacks\n3: Calculate # of stacks to items\n"))
 
-        if input == 1: #ping
+while True:
+
+    basic.clear
+
+    try:
+        iput = int(input("1: Ping server\n2: Calculate # of items into stacks\n3: Calculate # of stacks to items\n"))
+
+        if iput == 1: #ping
             if ping(input("Server to ping?\n"),4,2) == True:
                 print("Your server is online!")
             else:
                 print("Your server is offline.")
-        elif input == 2: # #>Stacks
-            input = input("# of items?")
-            print(f"Stacks: {math.floor(input/64)} Items: {input%64}")
-    except(ValueError):
+
+        elif iput == 2: #>Stacks
+            iput = int(input("# of items?\n"))
+            print(f"Stacks: {(iput//64)} Items: {iput%64}")
+        
+        elif iput == 3: #stacks>items
+            iput = input("# of stacks\n")
+            iput1 = input("# of items\n")
+            print(f"You have {iput*64+iput1} items")
+        
+        else: #bad input
+            raise(ValueError)
+
+    except Exception as exc: #fix bad input
+        print(f"DEBUG: {exc}")
         print("Oops, thats not a valid choice, try again.")
