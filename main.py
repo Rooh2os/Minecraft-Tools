c84f348fff930e0cce65323ea18a1f0564ae179e
Rooh2os <117789258+Rooh2os@users.noreply.github.com>
2025-08-08 10:15:10 -0500

    Changed image viewer code Added "Save individual icons" option Added resorce pack template creation for 1.14+

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import basic,requests,base64
+import basic,requests,base64,zipfile
+from PIL import Image
 from pythonping import ping as pping
 
 
@@ -45,6 +46,7 @@ try: #get/make settings data
 except(FileNotFoundError):
     settings = {
         "Open server icon": True,
+        "Save individual icons": False,
         "Use advanced ping": True,
         "Max saved servers": 10,
         "Starting value for lists": "    ",
@@ -54,12 +56,17 @@ except(FileNotFoundError):
     basic.json_write("settings.txt",settings,4)
     print("No settings found, making new settings.")
 
+#try: #make pack folder
+#    basic.os.mkdir("packs")
+#    print("No packs folder found. Making packs folder")
+#except(FileExistsError):
+#    pass
 
 while True:
 
 
     try:
-        iput = int(input("1: Ping server\n2: Calculate # of items into stacks\n3: Calculate # of stacks to items\n"))
+        iput = int(input("1: Ping server\n2: Make a template resorce pack\n3: Calculate # of items into stacks\n4: Calculate # of stacks to items\n"))
 
         basic.clear()
 
@@ -125,26 +132,124 @@ while True:
                     if "icon" in data:
                         imgdata = str(data["icon"])
                         imgdata = str(imgdata.split(",")[1])
-                        with open("icon.png","bw") as f:
-                            f.write(base64.b64decode(imgdata))
-                        print("The server icon has been saved as icon.png")
-                        if settings["Open server icon"] == True:
-                            basic.os.startfile("icon.png")
+
+                        if settings["Save individual icons"]:
+                            try:
+                                basic.os.mkdir("icons")
+                            except(FileExistsError):
+                                pass
+                            with open(f"icons/{iput}.png","wb") as f:
+                                f.write(base64.b64decode(imgdata))
+                            
+                            print(f"The server icon has been saved as {iput}.png")
+                            if settings["Open server icon"] == True:
+                                icon = Image.open(f"icons/{iput}.png")
+                                icon.show()
+                                #basic.os.startfile(f"icons/{iput}.png")
+                        else:
+                            with open("icon.png","wb") as f:
+                                f.write(base64.b64decode(imgdata))
+                        
+                            print("The server icon has been saved as icon.png")
+                            if settings["Open server icon"] == True:
+                                icon = Image.open("icon.png")
+                                icon.show()
+                                #basic.os.startfile("icon.png")
                             
                 else:
                     print("Your server is offline.")
 
-        elif iput == 2: #>Stacks
+        elif iput == 2: #make pack
+            iput = input("Version name?\n")
+
+            data = (requests.get("https://piston-meta.mojang.com/mc/game/version_manifest.json"))
+
+            if settings["Debug mode"] == True:
+                print(data.json())
+
+            data = (data.json())
+
+            if settings["Debug mode"] == True:
+                print(data)
+            data = data["versions"]
+            
+            iput1 = int(0)
+            for v in data:
+                if v["id"] == iput:
+                    break
+                if settings["Debug mode"]:
+                    print(v["id"])
+                iput1 += 1
+
+            if settings["Debug mode"]:
+                print(iput1)
+
+            if settings["Debug mode"]:
+                print(len(data))
+            if iput1+1 > (len(data)):
+                print("Could not find your version. Check the spelling.")
+                raise(ValueError)
+            
+            #data = (requests.get(requests.get(data[iput1]["url"])["downloads"]["client"]["url"]))
+            data = requests.get(data[iput1]["url"])
+            data = requests.get(data.json()["downloads"]["client"]["url"])
+
+            if settings["Debug mode"] == True:
+                print(data.text)
+
+            with open("temp","wb") as f:
+                f.write(data.content)
+
+
+            with zipfile.ZipFile("temp","r") as zip:
+                if settings["Debug mode"]:
+                    print(zip.namelist())
+                zip.extract("version.json",".")
+                basic.os.rename("version.json","temp1")
+                for file in zip.namelist():
+                    if file.startswith("assets/"):
+                        zip.extract(file,f"./packs/{iput}")
+
+            mcmeta = (basic.json_read("temp1"))
+            
+            try:
+                mcmeta = {
+                    "pack":{
+                        "description": "Made with Minecraft Tools by Rooh2os",
+                        "pack_format": (mcmeta["pack_version"]["data"])
+                    }
+                }
+            except(TypeError):
+                mcmeta = {
+                    "pack":{
+                        "description": "Made with Minecraft Tools by Rooh2os",
+                        "pack_format": (mcmeta["pack_version"])
+                    }
+                }
+            
+            basic.json_write(f"packs/{iput}/pack.mcmeta",mcmeta,4)
+
+            basic.os.remove("temp")
+            basic.os.remove("temp1")
+
+            print(f"Template pack for {iput} made sucessfully")
+
+        elif iput == 3: #>Stacks
             iput = int(input("# of items?\n"))
             print(f"Stacks: {(iput//64)} Items: {iput%64}")
         
-        elif iput == 3: #stacks>items
+        elif iput == 4: #stacks>items
             iput = int(input("# of stacks\n"))
             iput1 = int(input("# of items\n"))
             print(f"You have {iput1 + iput * 64} items")
         
+        
         else: #bad input
             raise(ValueError)
     
+    except(KeyError):
+        basic.os.remove("temp")
+        print("You must use a version above 1.14")
+
     except(ValueError,IndexError): #fix bad input
         print("Oops, thats not a valid choice, try again.")
